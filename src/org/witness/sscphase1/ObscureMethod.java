f1a6acd6424907e19b81935de7c40099d4f14b1f
vanevery <vanevery@walking-productions.com>
2011-02-01 01:10:14 -0500

    Added Canvas to arguments

--- src/org/witness/sscphase1/ObscureMethod.java
+++ src/org/witness/sscphase1/ObscureMethod.java
@@ -1,7 +1,8 @@
 package org.witness.sscphase1;
 
+import android.graphics.Canvas;
 import android.graphics.Rect;
 
 public interface ObscureMethod {
-	void obscureRect(Rect rect);
+	void obscureRect(Rect rect, Canvas canvas);
 }
