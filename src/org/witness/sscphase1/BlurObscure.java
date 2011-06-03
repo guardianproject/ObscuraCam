b1c3d349b95e04bd063464607befc3cca9dce641
vanevery <vanevery@walking-productions.com>
2011-06-03 14:26:05 -0400

    Formatting

--- src/org/witness/sscphase1/BlurObscure.java
+++ src/org/witness/sscphase1/BlurObscure.java
@@ -8,7 +8,6 @@ import android.app.Activity;
 import android.graphics.Bitmap;
 import android.graphics.Canvas;
 import android.graphics.Color;
-import android.graphics.Paint;
 import android.graphics.Rect;
 
 public class BlurObscure extends Activity implements ObscureMethod {
