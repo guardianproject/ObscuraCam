e564677dcebfdcac30658ea394b072afb51f4412
vanevery <vanevery@walking-productions.com>
2011-06-03 14:27:15 -0400

    Formatting

--- src/org/witness/sscphase1/PixelizeObscure.java
+++ src/org/witness/sscphase1/PixelizeObscure.java
@@ -7,8 +7,6 @@ package org.witness.sscphase1;
 import android.app.Activity;
 import android.graphics.Bitmap;
 import android.graphics.Canvas;
-import android.graphics.Color;
-import android.graphics.Paint;
 import android.graphics.Rect;
 
 public class PixelizeObscure extends Activity implements ObscureMethod {
