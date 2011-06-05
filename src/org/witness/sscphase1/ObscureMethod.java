977c0fc54129b36a228e3437c995f0e8072a8d28
vanevery <vanevery@walking-productions.com>
2011-06-05 10:59:44 -0400

    Cleanup

--- src/org/witness/sscphase1/ObscureMethod.java
+++ src/org/witness/sscphase1/ObscureMethod.java
@@ -2,6 +2,7 @@ package org.witness.sscphase1;
 
 import android.graphics.Canvas;
 import android.graphics.Rect;
+import android.graphics.RectF;
 
 public interface ObscureMethod {
 	void obscureRect(Rect rect, Canvas canvas);
