fee3e76f90c13a19bdf38ffeeb3a9b508ae2bbf8
Nathan Freitas <nathan@freitas.net>
2011-06-11 00:51:24 -0400

    fixed a divide by zero bug if the region is too small

--- src/org/witness/sscphase1/PixelizeObscure.java
+++ src/org/witness/sscphase1/PixelizeObscure.java
@@ -23,11 +23,15 @@ public class PixelizeObscure extends Activity implements ObscureMethod {
 	
 		int pixelSize = (rect.right-rect.left)/PIXEL_BLOCK;
 		
+		if (pixelSize <= 0) //1 is the smallest it can be
+			pixelSize = 1;
+		
 		pixelate(rect, pixelSize);
 	}
 	
 	private void pixelate(Rect rect, int pixelSize)
 	{
+		
 		if (rect.left <= 0) {
 			rect.left = 1;
 		} else if (rect.right >= originalBmp.getWidth()-1) {
@@ -47,8 +51,16 @@ public class PixelizeObscure extends Activity implements ObscureMethod {
 				
 				px = (x/pixelSize)*pixelSize;
 				py = (y/pixelSize)*pixelSize;
-				
-				originalBmp.setPixel(x, y, originalBmp.getPixel(px,py));
+
+				try
+				{ 
+					originalBmp.setPixel(x, y, originalBmp.getPixel(px,py));
+				}
+				catch (IllegalArgumentException iae)
+				{
+					//something is wrong with our pixel math
+					break; //stop the filter
+				}
 			}
 		}
 	}
