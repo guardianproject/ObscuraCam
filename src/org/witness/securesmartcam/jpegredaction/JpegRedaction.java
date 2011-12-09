d5b2f65cfe7e27d61b72e7fc7c057baec6380079
Nathan Freitas <nathan@freitas.net>
2011-12-09 00:43:16 -0500

    properly support redactRegions native/multi mode

--- src/org/witness/securesmartcam/jpegredaction/JpegRedaction.java
+++ src/org/witness/securesmartcam/jpegredaction/JpegRedaction.java
@@ -1,10 +1,13 @@
 package org.witness.securesmartcam.jpegredaction;
 
 import java.io.File;
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.Iterator;
 import java.util.Map;
 import java.util.Properties;
 
+import org.witness.securesmartcam.ImageRegion;
 import org.witness.securesmartcam.filters.CrowdPixelizeObscure;
 import org.witness.securesmartcam.filters.PixelizeObscure;
 import org.witness.securesmartcam.filters.RegionProcesser;
@@ -13,6 +16,7 @@ import org.witness.securesmartcam.filters.SolidObscure;
 import android.graphics.Bitmap;
 import android.graphics.Canvas;
 import android.graphics.RectF;
+import android.util.Log;
 
 public class JpegRedaction implements RegionProcesser {
 	
@@ -86,7 +90,12 @@ public class JpegRedaction implements RegionProcesser {
 			 String strInFile = mInFile.getAbsolutePath();
 			 String strOutFile = mOutFile.getAbsolutePath();
 	
-			 redactRegion(strInFile, strOutFile, (int)rect.left, (int)rect.right, (int)rect.top, (int)rect.bottom, mMethod);
+			 int left = Math.max(0, (int)rect.left);
+			 int right = Math.min(canvas.getWidth(),(int)rect.right);
+			 int top = Math.max(0, (int)rect.top);
+			 int bottom = Math.max(canvas.getHeight(), (int)rect.bottom);
+			 
+			 redactRegion(strInFile, strOutFile, left, right, top, bottom, mMethod);
 		     
 		     // return properties and data as a map
 		     mProps.put("initialCoordinates", "[" + rect.top + "," + rect.left + "]");
@@ -96,11 +105,64 @@ public class JpegRedaction implements RegionProcesser {
 		
 	}
 	
-	public void obscureRegions(String regions) {
+	public void processRegions(ArrayList<ImageRegion> iRegions, float inSampleSize, Canvas canvas) {
 
 		 String strInFile = mInFile.getAbsolutePath();
 		 String strOutFile = mOutFile.getAbsolutePath();
-	     redactRegions(strInFile, strOutFile, regions);
+		 
+		 //l,r,t,b[:method];
+		 StringBuffer sbRegions = new StringBuffer();
+		 
+		for (ImageRegion currentRegion : iRegions)
+	    {
+			RegionProcesser rProc = currentRegion.getRegionProcessor();
+			
+            RectF rect = new RectF(currentRegion.getBounds());
+            rect.left *= inSampleSize;
+            rect.top *= inSampleSize;
+            rect.right *= inSampleSize;
+            rect.bottom *= inSampleSize;
+
+			 int left = Math.max(0, (int)rect.left);
+			 int right = Math.min(canvas.getWidth(),(int)rect.right);
+			 int top = Math.max(0, (int)rect.top);
+			 int bottom = Math.min(canvas.getHeight(), (int)rect.bottom);
+            
+            sbRegions.append(left);
+            sbRegions.append(',');
+
+            sbRegions.append(right);
+            sbRegions.append(',');
+
+            sbRegions.append(top);
+            sbRegions.append(',');
+
+            sbRegions.append(bottom);
+            sbRegions.append(':');
+
+            if (rProc instanceof CrowdPixelizeObscure)
+        	{
+        		mMethod = METHOD_INVERSE_PIXELLATE;
+        	}
+        	else if (rProc instanceof PixelizeObscure)
+        	{
+        		mMethod = METHOD_PIXELLATE;
+        	}
+        	else if (rProc instanceof SolidObscure)
+        	{
+        		mMethod = METHOD_SOLID;
+        	}
+            
+            sbRegions.append(mMethod);
+            sbRegions.append(';');
+            
+	    
+	    }
+
+		Log.d("jpegredaction:size", canvas.getWidth() + "x" + canvas.getHeight());
+		Log.d("jpegredaction:regions", sbRegions.toString());
+		
+	    redactRegions(strInFile, strOutFile, sbRegions.toString());
 		
 	}
 	
