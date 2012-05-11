d671c9bd32927d25fbca11c879089db5c1957288
n8fr8 <nathan@freitas.net>
2012-05-10 20:21:55 -0400

    adding video output preferences

---
+++ src/org/witness/ssc/video/VideoPreferences.java
@@ -0,0 +1,52 @@
+/* Copyright (c) 2009, Nathan Freitas, Orbot / The Guardian Project - http://openideals.com/guardian */
+/* See LICENSE for licensing information */
+
+package org.witness.ssc.video;
+
+import org.witness.sscphase1.R;
+
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.preference.PreferenceActivity;
+import android.preference.PreferenceManager;
+
+
+public class VideoPreferences 
+		extends PreferenceActivity  {
+
+	
+	
+	protected void onCreate(Bundle savedInstanceState)
+	{
+		super.onCreate(savedInstanceState);
+		addPreferencesFromResource(R.xml.videopreferences);
+		
+		SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(this);
+		
+	}
+	
+	
+	@Override
+	protected void onResume() {
+	
+		super.onResume();
+	
+		
+		
+	};
+	
+	
+	
+	
+	/* (non-Javadoc)
+	 * @see android.app.Activity#onStop()
+	 */
+	@Override
+	protected void onStop() {
+		super.onStop();
+		
+		//Log.d(getClass().getName(),"Exiting Preferences");
+	}
+
+	
+}
