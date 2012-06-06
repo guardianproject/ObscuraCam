e4edb9220a874b7124d1b96887b57a5debe80dcf
n8fr8 <nathan@freitas.net>
2012-06-06 10:26:05 -0400

    added region trail collection

---
+++ src/org/witness/ssc/video/RegionTrail.java
@@ -0,0 +1,84 @@
+package org.witness.ssc.video;
+
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.Hashtable;
+import java.util.Iterator;
+import java.util.Set;
+import java.util.TreeSet;
+
+public class RegionTrail {
+
+	
+	private HashMap<Integer,ObscureRegion> regionMap = new HashMap<Integer,ObscureRegion>();
+	
+	private int startTime = 0;
+	private int endTime = 0;
+	
+	public RegionTrail (int startTime, int endTime)
+	{
+		this.startTime = startTime;
+		this.endTime = endTime;
+	}
+	
+	public int getStartTime() {
+		return startTime;
+	}
+
+	public void setStartTime(int startTime) {
+		this.startTime = startTime;
+	}
+
+	public int getEndTime() {
+		return endTime;
+	}
+
+	public void setEndTime(int endTime) {
+		this.endTime = endTime;
+	}
+
+	public void addRegion (ObscureRegion or)
+	{
+		regionMap.put(or.timeStamp,or);
+		or.setRegionTrail(this);
+	}
+	
+	public Iterator<ObscureRegion> getRegionsIterator ()
+	{
+		return regionMap.values().iterator();
+	}
+	
+	public Collection<ObscureRegion> getRegions ()
+	{
+		return regionMap.values();
+	}
+	
+	public ObscureRegion getCurrentRegion (int time)
+	{
+		
+		if (time < startTime || time > endTime)
+			return null;
+		else if (regionMap.size() > 0)
+		{
+		
+			
+			TreeSet<Integer> regionKeys = new TreeSet<Integer>(regionMap.keySet());
+			
+			Integer lastRegionKey = -1;
+			
+			for (Integer regionKey:regionKeys)
+			{
+				int comp = regionKey.compareTo(time);
+				lastRegionKey = regionKey;
+				
+				if (comp == 0 || comp == 1)
+					break; //it is a match!
+				
+			}
+			
+			return regionMap.get(lastRegionKey);
+		}
+		else
+			return null;
+	}
+}
