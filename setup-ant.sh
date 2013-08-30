ea53b2bac85bfb37ea138db1184a3fbb74294055
Hans-Christoph Steiner <hans@eds.org>
2013-08-30 18:27:57 -0400

    update script for setting up command line builds to standard setup-ant.sh

---
+++ setup-ant.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+if ! type -P android &> /dev/null; then
+    echo "Error: 'android' utility is not in your path."
+    echo "  Did you forget to setup the SDK?"
+    exit 1
+fi
+
+projectname=`sed -n 's,.*name="app_name">\(.*\)<.*,\1,p' res/values/strings.xml`
+
+android update project --path . --name $projectname --subprojects
--- update-ant-build.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-
-# make sure your Android SDK tools path is set in SDK_BASE
-android update project --path . --name SecureSmartCam
-
