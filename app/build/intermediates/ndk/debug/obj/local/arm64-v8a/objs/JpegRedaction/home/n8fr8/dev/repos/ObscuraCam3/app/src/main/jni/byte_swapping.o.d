dc4b047f39d1b9315df766d133e80dd7af41a6eb
n8fr8 <nathan@freitas.net>
2015-08-15 00:24:17 -0400

    remove build files

--- app/build/intermediates/ndk/debug/obj/local/arm64-v8a/objs/JpegRedaction/home/n8fr8/dev/repos/ObscuraCam3/app/src/main/jni/byte_swapping.o.d
+++
@@ -1,5 +0,0 @@
-/home/n8fr8/dev/repos/ObscuraCam3/app/build/intermediates/ndk/debug/obj/local/arm64-v8a/objs/JpegRedaction//home/n8fr8/dev/repos/ObscuraCam3/app/src/main/jni/byte_swapping.o: \
- /home/n8fr8/dev/repos/ObscuraCam3/app/src/main/jni/byte_swapping.cpp \
- /home/n8fr8/dev/repos/ObscuraCam3/app/src/main/jni/byte_swapping.h
-
-/home/n8fr8/dev/repos/ObscuraCam3/app/src/main/jni/byte_swapping.h:
--- app/build/intermediates/ndk/debug/obj/local/armeabi-v7a/objs/JpegRedaction/home/n8fr8/dev/repos/ObscuraCam3/app/src/main/jni/debug_flag.o.d
+++
@@ -1,5 +0,0 @@
-/home/n8fr8/dev/repos/ObscuraCam3/app/build/intermediates/ndk/debug/obj/local/armeabi-v7a/objs/JpegRedaction//home/n8fr8/dev/repos/ObscuraCam3/app/src/main/jni/debug_flag.o: \
- /home/n8fr8/dev/repos/ObscuraCam3/app/src/main/jni/debug_flag.cpp \
- /home/n8fr8/dev/repos/ObscuraCam3/app/src/main/jni/debug_flag.h
-
-/home/n8fr8/dev/repos/ObscuraCam3/app/src/main/jni/debug_flag.h:
