8b9b9d2a9a764a4736dcf44fd7733ae2e52dafbf
N-Pex <npexdev@gmail.com>
2017-07-14 10:32:02 +0200

    Update build files and library versions

--- setup-gradle.sh
+++ setup-gradle.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-if ! type -P android &> /dev/null; then
-    echo "Error: 'android' utility is not in your path."
-    echo "  Did you forget to setup the SDK?"
-    exit 1
-fi
+#if ! type -P android &> /dev/null; then
+#    echo "Error: 'android' utility is not in your path."
+#    echo "  Did you forget to setup the SDK?"
+#    exit 1
+#fi
 
 projectname=`sed -n 's,.*name="app_name">\(.*\)<.*,\1,p' res/values/strings.xml`
 
-android update project --path . --name $projectname --subprojects
+/Applications/adt-bundle-mac-x86_64-20140702/sdk/tools/android update project --path . --name $projectname --subprojects
