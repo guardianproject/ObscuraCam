5cefd33a0e189a651c09a233f52f80561b2ed823
N-Pex <npexdev@gmail.com>
2017-07-24 12:15:57 +0200

    Build native code with new build tools

--- app/src/main/jni/Android.mk
+++ app/src/main/jni/Android.mk
@@ -9,6 +9,7 @@ LOCAL_SRC_FILES := org_witness_securesmartcam_jpegredaction_JpegRedaction.cpp \
 			iptc.cpp jpeg.cpp  tiff_ifd.cpp tiff_tag.cpp jpeg_decoder.cpp \
         		byte_swapping.cpp jpeg_marker.cpp debug_flag.cpp
 
+LOCAL_CPP_FEATURES += exceptions
 
 include $(BUILD_SHARED_LIBRARY)
 
