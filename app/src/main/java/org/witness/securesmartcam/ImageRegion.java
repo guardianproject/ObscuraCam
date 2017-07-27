43a8fc49e869ba35b29e8547e8ac0bcf6f631223
N-Pex <npexdev@gmail.com>
2017-07-27 21:57:41 +0200

    Cleanup

--- app/src/main/java/org/witness/securesmartcam/ImageRegion.java
+++ app/src/main/java/org/witness/securesmartcam/ImageRegion.java
@@ -1,65 +1,34 @@
 package org.witness.securesmartcam;
 
-import net.londatiga.android.ActionItem;
-import net.londatiga.android.QuickAction;
-import net.londatiga.android.QuickAction.OnActionItemClickListener;
-
-import org.witness.securesmartcam.filters.BlurObscure;
-import org.witness.securesmartcam.filters.ConsentTagger;
-import org.witness.securesmartcam.filters.CrowdPixelizeObscure;
-import org.witness.securesmartcam.filters.MaskObscure;
-import org.witness.securesmartcam.filters.SolidObscure;
-import org.witness.securesmartcam.filters.PixelizeObscure;
-import org.witness.securesmartcam.filters.RegionProcesser;
-import org.witness.sscphase1.ObscuraApp;
-import org.witness.sscphase1.R;
-
 import android.content.res.Resources;
 import android.graphics.Matrix;
 import android.graphics.PointF;
 import android.graphics.RectF;
 import android.graphics.drawable.Drawable;
-import android.os.Handler;
-import android.os.Message;
-import android.os.Parcel;
-import android.os.Parcelable;
 import android.util.Log;
 import android.util.TypedValue;
-import android.view.LayoutInflater;
 import android.view.MotionEvent;
-import android.view.ScaleGestureDetector;
 import android.view.View;
-import android.view.View.OnTouchListener;
-import android.view.ViewConfiguration;
-import android.widget.FrameLayout;
-import android.widget.PopupWindow.OnDismissListener;
-import android.widget.RelativeLayout;
 
-public class ImageRegion implements OnActionItemClickListener 
+import org.witness.securesmartcam.filters.BlurObscure;
+import org.witness.securesmartcam.filters.ConsentTagger;
+import org.witness.securesmartcam.filters.CrowdPixelizeObscure;
+import org.witness.securesmartcam.filters.MaskObscure;
+import org.witness.securesmartcam.filters.PixelizeObscure;
+import org.witness.securesmartcam.filters.RegionProcesser;
+import org.witness.securesmartcam.filters.SolidObscure;
+import org.witness.sscphase1.ObscuraApp;
+import org.witness.sscphase1.R;
+
+public class ImageRegion
 {
-
 	public static final String LOGTAG = "SSC.ImageRegion";
 	
 	// Rect for this when unscaled
 	public RectF mBounds;
-	//public RectF mTmpBounds;
 	
 	// Start point for touch events
 	PointF mStartPoint = null;
-	PointF mNonMappedStartPoint = null;
-	
-	// Our current mode
-	public static final int NORMAL_MODE = 0;
-	public static final int EDIT_MODE = 1;
-	
-// The current touch event mode
-	public final static int NONE = 0;
-	public final static int MOVE = 1;
-	
-
-	// What should be done to this region
-	public static final int NOTHING = 0;
-	public static final int OBSCURE = 1;
 	
 	public static final int REDACT = 0; // PaintSquareObscure
 	public static final int PIXELATE = 1; // PixelizeObscure
@@ -85,22 +54,11 @@ public class ImageRegion implements OnActionItemClickListener
 	 */
 	int mObscureType = PIXELATE;
 
-	//the other of these strings and resources determines the order in the popup menu
-	private final static String[] mFilterLabels = {"Redact","Pixelate","CrowdPixel","Mask"};
-	private final static int[] mFilterIcons = {R.drawable.ic_context_fill,R.drawable.ic_context_pixelate,R.drawable.ic_context_pixelate, R.drawable.ic_context_mask, R.drawable.ic_context_id};
-
 	public final Drawable unidentifiedBorder, identifiedBorder;
 	public Drawable imageRegionBorder;
 	
 	// The ImageEditor object that contains us
 	ImageEditor mImageEditor;
-	
-	// Popup menu for region 
-	// What's the license for this?
-	QuickAction mPopupMenu;
-		
-	ActionItem[] mActionFilters;
-	
 	RegionProcesser mRProc;
 	
 	private final static float MIN_MOVE = 5f;
@@ -218,7 +176,6 @@ public class ImageRegion implements OnActionItemClickListener
 			float left, float top, 
 			float right, float bottom, Matrix matrix) 
 	{
-		//super(imageEditor);
 		super();
 
 		// Handle
@@ -235,52 +192,9 @@ public class ImageRegion implements OnActionItemClickListener
 		mMatrix = matrix;
 		iMatrix = new Matrix();
     	mMatrix.invert(iMatrix);
-		
-		// Calculate the minMoveDistance using the screen density
-		//float scale = this.getResources().getDisplayMetrics().density;
-	//	minMoveDistance = minMoveDistanceDP * scale + 0.5f;
-		
-		
+
 		mBounds = new RectF(left, top, right, bottom);	
-		
-				
-		// Inflate Layout
-		// imageregioninner is a FrameLayout
-		/*
-		LayoutInflater inflater = LayoutInflater.from(mImageEditor);        
-		inflater.inflate(R.layout.imageregioninner, this, true);
-		setBackgroundDrawable(mImageEditor.getResources().getDrawable(R.drawable.border));
-		updateMatrix();
 
-        mMoveRegion = (View)findViewById(R.id.MoveRegion);
-
-        // Setting the onTouchListener for the moveRegion
-        // Might also want to do this for the other views (corners)
-<<<<<<< HEAD
-        mMoveRegion.setOnTouchListener(this);
-                
-=======
-        moveRegion.setOnTouchListener(this);
-        
-        imageRegionBorder = unidentifiedBorder;
->>>>>>> informav1
-        initPopup();
-        
-        // This doesn't work with the touch listener always returning true.  
-        // In some cases touch listener returns false and this gets triggered
-       
-        mMoveRegion.setOnClickListener(new OnClickListener (){
-
-			// @Override
-			public void onClick(View v)
-			{
-				Log.v(LOGTAG,"onClick");
-				
-				inflatePopup(false);
-			}
-			
-		});*/
-        
         //set default processor
         this.setRegionProcessor(new PixelizeObscure());
     }		
@@ -291,73 +205,7 @@ public class ImageRegion implements OnActionItemClickListener
 		iMatrix = new Matrix();
     	mMatrix.invert(iMatrix);
 	}
-	
-	public void inflatePopup(boolean showDelayed) {
 
-		if (mPopupMenu == null)
-			initPopup();
-		
-		
-		if (showDelayed) {
-			// We need layout to pass again, let's wait a second or two
-			new Handler() {
-				@Override
-				 public void handleMessage(Message msg) {
-
-					//float[] points = {mBounds.centerX(), mBounds.centerY()};		
-					//mMatrix.mapPoints(points);
-					
-			
-					//mPopupMenu.show(mImageEditor.getImageView(), (int)points[0], (int)points[1]);
-					mPopupMenu.show(mImageEditor.getImageView(), (int)mBounds.centerX(), (int)mBounds.centerY());
-			        }
-			}.sendMessageDelayed(new Message(), 300);
-		} else {			
-
-			float[] points = {mBounds.centerX(), mBounds.centerY()};		
-			mMatrix.mapPoints(points);
-			
-			//mPopupMenu.show(mImageEditor.getImageView(), (int)points[0], (int)points[1]);
-			mPopupMenu.show(mImageEditor.getImageView(), (int)mBounds.centerX(), (int)mBounds.centerY());
-		}
-		
-
-	}
-	
-	private void initPopup ()
-	{
-		mPopupMenu = new QuickAction(mImageEditor);
-//		mPopupMenu.setOnDismissListener(new OnDismissListener() {
-//			@Override
-//			public void onDismiss() {
-//				setSelected(false);
-//				mImageEditor.forceUpdateDisplayImage();
-//			}
-//		});
-		ActionItem aItem;
-		
-		for (int i = 0; i < mFilterLabels.length; i++)
-		{
-		
-			aItem = new ActionItem();
-			aItem.setTitle(mFilterLabels[i]);
-			
-			aItem.setIcon(mImageEditor.getResources().getDrawable(mFilterIcons[i]));			
-			
-			mPopupMenu.addActionItem(aItem);
-
-		}
-		
-		aItem = new ActionItem();
-		aItem.setTitle("Clear Tag");
-		aItem.setIcon(mImageEditor.getResources().getDrawable(R.drawable.ic_context_delete));
-
-		mPopupMenu.addActionItem(aItem);
-
-		mPopupMenu.setOnActionItemClickListener(this);
-	}
-	
-	
 	boolean isSelected ()
 	{
 		return selected;
@@ -376,22 +224,7 @@ public class ImageRegion implements OnActionItemClickListener
 		
 		//updateLayout();
 	}
-	
-	float scaleX, scaleY, leftOffset, topOffset;
-	
-	public void updateMatrix ()
-	{
-		float[] mValues = new float[9];
-		mMatrix.getValues(mValues);		
-    	mMatrix.invert(iMatrix);
-		scaleX = mValues[Matrix.MSCALE_X];
-		scaleY = mValues[Matrix.MSCALE_Y];
-		
-		leftOffset = mValues[Matrix.MTRANS_X];
-		topOffset = mValues[Matrix.MTRANS_Y];
-		
-	}
-	
+
 	public RectF getBounds ()
 	{
 		return mBounds;
@@ -599,26 +432,4 @@ public class ImageRegion implements OnActionItemClickListener
 		else
 			imageRegionBorder = unidentifiedBorder;
 	}
-
-	@Override
-	public void onItemClick(QuickAction source, int pos, int actionId) {
-		
-		if (pos == mFilterLabels.length) //meaing after the last one
-		{
-        	mImageEditor.deleteRegion(ImageRegion.this);
-		}
-		else
-		{
-        	mObscureType = pos;
-        	updateRegionProcessor(mObscureType);
-        	
-        		
-		}
-
-		mImageEditor.updateDisplayImage();
-		
-	}
-
-	
-	
 }
